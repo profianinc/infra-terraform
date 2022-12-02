profianinc/infra-terraform

commit 2af67db719977fb1f75de0f743a9c41f09cf2997

Add branch protections for enarx/vfs:main
As requested by @rvolosatovs at https://chat.enarx.dev/channel/development?msg=77qjhfHx6tBKPJia5

Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

github_branchprotections_enarx.tf
@@ -1006,6 +1006,40 @@ resource "github_branch_protection" "enarx-vscode-enarx-_wild_" {
   }
 }
 
+resource "github_branch_protection" "enarx-vfs-main" {
+  provider = github.enarx
+  repository_id = github_repository.enarx-vfs.node_id
+
+  pattern = "main"
+
+  enforce_admins                  = true
+  require_signed_commits          = false
+  required_linear_history         = true
+  require_conversation_resolution = true
+  push_restrictions               = []
+  allows_deletions                = false
+  allows_force_pushes             = false
+  blocks_creations                = false
+
+  required_status_checks {
+    strict   = false
+    contexts = [
+      "nix fmt",
+      "build (ubuntu-latest, x86_64-linux, aarch64-unknown-linux-musl)",
+      "build (ubuntu-latest, x86_64-linux, x86_64-unknown-linux-musl)"
+    ]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    restrict_dismissals             = false
+    dismissal_restrictions          = []
+    pull_request_bypassers          = []
+    require_code_owner_reviews      = false
+    required_approving_review_count = 1
+  }
+}
+
 resource "github_branch_protection" "enarx-vfs-_wild_" {
   provider = github.enarx
   repository_id = github_repository.enarx-vfs.node_id

commit fedc2e1795fcf57052c3afde8eefd2ad4fc1df14

Fix branch protections for enarx/vfs:main
Adds missing checks.

Fixes: 2af67db719977fb1f75de0f743a9c41f09cf2997
Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

github_branchprotections_enarx.tf
@@ -1026,7 +1026,10 @@ resource "github_branch_protection" "enarx-vfs-main" {
     contexts = [
       "nix fmt",
       "build (ubuntu-latest, x86_64-linux, aarch64-unknown-linux-musl)",
-      "build (ubuntu-latest, x86_64-linux, x86_64-unknown-linux-musl)"
+      "build (ubuntu-latest, x86_64-linux, x86_64-unknown-linux-musl)",
+      "checks (ubuntu-latest, x86_64-linux, clippy)",
+      "checks (ubuntu-latest, x86_64-linux, nextest)",
+      "checks (ubuntu-latest, x86_64-linux, fmt)",
     ]
   }
 